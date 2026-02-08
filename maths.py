e9bff09d0007775bf0f3cdc0f9915dd8d209785c
MohitAgarwal17 <singhalmohitraj2@gmail.com>
2026-02-08 15:23:43 +0530

    Baic Python Maths

---
+++ maths.py
@@ -0,0 +1,24 @@
+import math
+
+x = 3.14
+y = 4
+z = 5
+
+# result = round(x)
+# result = abs(y)
+# result = pow(z, 3)
+# result =  max(x,y,z)
+result =  min(x,y,z)
+print(result)
+x = 9.9
+
+print(math.pi)
+print(math.e)
+# result = math.sqrt(x)
+# result = math.ceil(x)
+# result = math.floor(x)
+
+# result = math.pi * 3 ** 2
+
+result = math.sqrt(pow(z,2)+pow(y,2))
+print(f"{result: .2f}")
